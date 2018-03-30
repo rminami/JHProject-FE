4acd6dac5f8159883327f218046826250ac03d07
Ryosuke Minami <ryosukem13@gmail.com>
2018-03-30 19:13:15 +0100

    Connectivity with separate backend now works

--- src/components/FileBrowse.vue
+++ src/components/FileBrowse.vue
@@ -1,106 +1,128 @@
 <template>
     <v-container xs10 offset-xs1>
-        <v-list two-line subheader>
-            <v-subheader inset>Folders</v-subheader>
-            <v-list-tile avatar v-for="item in items" :key="item.title" @click="">
+        <v-list two-line>
+
+            <!-- List of directories in current path -->
+            <v-subheader v-if="dirs.length > 0" inset>Folders</v-subheader>
+            <v-list-tile avatar v-for="dir in dirs" :key="dir.id"
+                v-on:click="$router.push(dir.file_path)" @click="">
                 <v-list-tile-avatar>
-                    <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                    <v-icon :class="[dir.iconClass]">{{ dir.icon }}</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    <v-list-tile-title>{{ dir.file_name }}</v-list-tile-title>
                 </v-list-tile-content>
                 <v-list-tile-action>
                     <v-btn icon ripple>
-                        <v-icon color="grey lighten-1">info</v-icon>
+                        <v-icon color="grey lighten-1">more_vert</v-icon>
                     </v-btn>
                 </v-list-tile-action>
             </v-list-tile>
-            <v-divider inset></v-divider>
+
+            <!-- Divider -->
+            <v-divider inset v-if="dirs.length > 0"></v-divider>
+
+            <!-- List of files in current path -->
             <v-subheader inset>Files</v-subheader>
-            <v-list-tile v-for="item in items2" :key="item.title" avatar @click="">
+            <v-list-tile avatar v-for="file in files" :key="file.id" @click="">
                 <v-list-tile-avatar>
-                    <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                    <v-icon :class="[file.iconClass]">{{ file.icon }}</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    <v-list-tile-title>{{ file.file_name }}</v-list-tile-title>
                 </v-list-tile-content>
                 <v-list-tile-action>
                     <v-btn icon ripple>
-                        <v-icon color="grey lighten-1">info</v-icon>
+                        <v-icon color="grey lighten-1">more_vert</v-icon>
                     </v-btn>
                 </v-list-tile-action>
             </v-list-tile>
+
         </v-list>
     </v-container>
 </template>
 
 <script>
-// import * as axios from 'axios'
-// import * as path from 'path'
+import * as axios from 'axios'
+import * as url from 'url'
 
-// const BE_ENDPOINT = 'http://127.0.0.1:3000/'
-
-// const attachIcon = (entry) => {
-//     const res = Object.assign({}, entry)
-//     switch (res.type) {
-//     case 'directory':
-//         res.icon = 'folder'
-//         return res
-//     case 'tabular':
-//         res.icon = 'database'
-//         return res
-//     case 'scalable_image':
-//         res.icon = 'image'
-//         return res
-//     default:
-//         res.icon = 'file'
-//         return res
-//     }
-// }
+const SERVER_URL = 'http://127.0.0.1:3000/'
 
 export default {
-    data() {
-        return {
-            items: [
-                { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Photos', subtitle: 'Jan 9, 2014' },
-                { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Recipes', subtitle: 'Jan 17, 2014' },
-                { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Work', subtitle: 'Jan 28, 2014' },
-            ],
-            items2: [
-                { icon: 'storage', iconClass: 'blue white--text', title: 'processed-data.csv', subtitle: 'Jan 20, 2014' },
-                { icon: 'image', iconClass: 'amber white--text', title: 'slide_002.dzi', subtitle: 'Jan 10, 2014' },
-            ],
-        }
+    data: () => ({
+        dirs: [],
+        files: [],
+    }),
+
+    created() {
+        console.log(this.$route.path)
+        this.getFiles()
     },
-    // computed() {
-    //     async () => axios.get(path.join(BE_ENDPOINT, 'files'), {
-    //         params: {
-    //             view: 'meta',
-    //             include_children: true,
-    //         },
-    //     }).then((res) => {
-    //         res.data.children.forEach(attachIcon)
-    //         return res.data
-    //     })
-    // },
+
+    watch: {
+        $route: 'getFiles',
+    },
+
+    methods: {
+        getFiles() {
+            axios({
+                url: url.resolve(SERVER_URL, this.$route.path),
+                responseType: 'json',
+                params: {
+                    view: 'meta',
+                    include_children: true,
+                },
+            })
+            .then(res => {
+                const processed = this.attachIcons(res.data.children)
+                this.dirs = processed.filter(item => item.type === 'directory')
+                this.files = processed.filter(item => item.type !== 'directory')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        attachIcons(items) {
+            return items.map(item => {
+                const res = item
+                switch (res.type) {
+                case 'directory':
+                    res.icon = 'folder'
+                    res.iconClass = 'grey lighten-1 white--text'
+                    return res
+                case 'tabular':
+                    res.icon = 'storage'
+                    res.iconClass = 'blue white--text'
+                    return res
+                case 'scalable_image':
+                    res.icon = 'image'
+                    res.iconClass = 'amber white--text'
+                    return res
+                default:
+                    res.icon = 'note'
+                    res.iconClass = 'teal white--text'
+                    return res
+                }
+            })
+        },
+    },
     name: 'Files',
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>

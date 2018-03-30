a640d1a9fdb4883e6c4e3153dde2759e94ee11ae
Ryosuke Minami <ryosukem13@gmail.com>
2018-03-30 19:13:55 +0100

    Trying to get links in navbar to work

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-navigation-drawer persistent v-model="drawer" disable-resize-watcher fixed app>
+            <v-list>
+                <v-list-tile value="true" v-for="(item, i) in items" :key="i" @click="" v-on:click="[this.$router.push('/')]">
+                    <v-list-tile-action>
+                        <v-icon v-html="item.icon"></v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
@@ -41,10 +29,6 @@ export default {
         return {
             drawer: false,
             items: [
-                {
-                    icon: 'bubble_chart',
-                    title: 'Inspire',
-                },
                 {
                     icon: 'assignment',
                     title: 'Machine Learning',
@@ -60,3 +44,8 @@ export default {
     name: 'App',
 }
 </script>
+
+<style scoped>
+/* Put custom CSS here. */
+</style>
+

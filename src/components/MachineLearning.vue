453819b8768edae1d64b4415117f95dac91260a8
Ryosuke Minami <ryosukem13@gmail.com>
2018-03-31 04:39:13 +0100

    Added new components

---
+++ src/components/MachineLearning.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <v-flex xs12 class="text-xs-center" mt-5>
+                <h1>Machine Learning</h1>
+                <p>Let's get started.</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
---
+++ src/components/Settings.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <v-flex xs12 class="text-xs-center" mt-5>
+                <h1>Settings</h1>
+                <p>Someone wanna work on this?</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+

074636cb5e4ca4e89c6c607f63705e80fe85e3bf
Ryosuke Minami <ryosukem13@gmail.com>
2018-03-31 04:38:44 +0100

    Added routes for admin console, settings, etc.

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import FileBrowse from '@/components/FileBrowse'
+
+const routerOptions = [
+    {
+        path: '/files*',
+        component: 'FileBrowse',
+    },
+    {
+        path: '/ml',
+        component: 'MachineLearning',
+    },
+    {
+        path: '/admin',
+        component: 'Admin',
+    },
+    {
+        path: '/settings',
+        component: 'Settings',
+    },
+    {
+        path: '/hello',
+        component: 'HelloWorld',
+    },
+]
+
+const routes = routerOptions.map(route => ({
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`),
+}))
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld,
-        },
-        {
-            path: '/files*',
-            name: 'FileBrowse',
-            component: FileBrowse,
-        },
-    ],
+    routes,
 })

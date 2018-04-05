72c8c262228293bf9cf4547fd1909601bd36349c
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-05 23:59:54 +0100

    Added placeholders for components to write later

---
+++ src/components/RawFileView.vue
@@ -0,0 +1,14 @@
+<template>
+<v-container fluid>
+  <v-layout column>
+    <v-flex xs12 class="text-xs-center" mt-5>
+      <h1>Raw file view</h1>
+      <p>Coming soon.</p>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {}
+</script>

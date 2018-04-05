9e48f4eed8f800dce1d583b64772b184e9207156
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-06 00:00:36 +0100

    Added component for box representing machine learning step

---
+++ src/components/MLBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-card width="220px">
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline">{{ boxTitle }}</h3>
+        </div>
+      </v-card-title>
+      <v-card-text>
+        <p>This is the text.</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn flat color="orange">Share</v-btn>
+        <v-btn flat color="orange" @click.stop="toggleFileModal">Explore</v-btn>
+        <file-input-modal :is-open="fileModalOpen"></file-input-modal>
+      </v-card-actions>
+    </v-card>
+  </div>
+  
+</template>
+
+<script>
+import FileInputModal from './FileInputModal'
+
+export default {
+  props: ['boxTitle'],
+  components: {
+    'file-input-modal': FileInputModal
+  },
+  data() {
+    return {
+      fileModalOpen: false
+    }
+  },
+  methods: {
+    toggleFileModal() {
+      this.fileModalOpen = !this.fileModalOpen
+    }
+  }
+}
+</script>

592125d87fee6febcdaaefd2a0fd89d4a1681d9e
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-05 14:21:45 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,26 @@
 <template>
-  <v-app>
-    <v-navigation-drawer persistent v-model="sidebar" :mini-variant.sync="mini"
-    disable-resize-watcher fixed app>
-      <v-list>
-        <v-list-tile v-for="(item, i) in items" :key="i" :to="item.path">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-        <v-expansion-panel expand>
-          <v-expansion-panel-content>
-            <div slot="header">
-              Admin Tools
-            </div>
-            <v-card>
-              <v-list-tile v-for="(adminItem, i) in adminItems" :key="i" :to="adminItem.path">
-                <v-list-tile-action>
-                  <v-icon>{{ adminItem.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>{{ adminItem.title }}</v-list-tile-content>
-              </v-list-tile>
-            </v-card>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="sidebar = !sidebar"></v-toolbar-side-icon>
-      <v-toolbar-title>
-        <router-link to="/files" tag="span" style="cursor: pointer">
-          {{ title }}
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      sidebar: false,
-      mini: false,
-      items: [
-        {
-          icon: 'home',
-          title: 'Files',
-          path: '/files'
-        },
-        {
-          icon: 'assignment',
-          title: 'Machine Learning',
-          path: '/ml'
-        },
-        {
-          icon: 'settings',
-          title: 'Settings',
-          path: '/settings'
-        },
-        {
-          icon: 'verified_user',
-          title: 'Admin Console',
-          path: '/admin/console'
-        }
-      ],
-      adminItems: [
-        {
-          icon: 'dashboard',
-          title: 'Dashboard',
-          path: '/admin/dashboard'
-        },
-        {
-          icon: 'people',
-          title: 'User Permissions',
-          path: '/admin/users'
-        },
-        {
-          icon: 'swap_vertical_circle',
-          title: 'Server Settings',
-          path: '/admin/server-settings'
-        },
-        {
-          icon: 'security',
-          title: 'Project Controls',
-          path: '/admin/project-controls'
-        }
-      ],
-      title: 'Digital Pathology Viewer'
-    }
-  },
-  name: 'App'
-}
-</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
 
-<style scoped>
-/* Put custom CSS here. */
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
 </style>

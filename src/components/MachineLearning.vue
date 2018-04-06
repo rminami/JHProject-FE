4c1409789b809c3698bb9df664d869623ec3ce31
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-06 15:49:51 +0100

    Organizing machine learning UI into components

--- src/components/MachineLearning.vue
+++ src/components/MachineLearning.vue
@@ -3,41 +3,71 @@
   <v-container fluid fill-height grid-list-xs style="padding-left: 0">
     <v-layout row>
       <v-container fluid grid-list-md>
-        <v-layout row wrap>
-          <v-flex xs12 class="text-xs-center" mt-5>
-            <h1>Machine Learning</h1>
-            <p>Let's get started.</p>
-          </v-flex>
-        </v-layout>
         <v-layout row>
-          <input-box box-title="Select input file"/>
-          <ml-box box-title="ML Step 1"/>
-          <ml-box box-title="ML Step 2"/>
+          <input-file-panel box-title="Select input file" />
+          <transformer-panel step-number="1" :algos="algos"/>
+          <next-panel/>
         </v-layout>
       </v-container>
     </v-layout>
   </v-container>
-  
+
 </div>
 </template>
 
 <script>
+import axios from 'axios'
+import url from 'url'
+
 import MLBox from './ml/MLBox'
-import InputBox from './ml/InputBox'
+import InputFilePanel from './ml/InputFilePanel'
+import EstimatorPanel from './ml/EstimatorPanel'
+import TransformerPanel from './ml/TransformerPanel'
+import NextPanel from './ml/NextPanel'
+
+const BE_ENDPOINT = 'http://127.0.0.1:4000'
+const ML_ENDPOINT = 'http://127.0.0.1:7000'
 
 export default {
   components: {
     'ml-box': MLBox,
-    'input-box': InputBox
+    'input-file-panel': InputFilePanel,
+    'next-panel': NextPanel,
+    'estimator-panel': EstimatorPanel,
+    'transformer-panel': TransformerPanel
   },
   data() {
     return {
+      currentJobSteps: [
+        {
+          type: 'InputFile'
+        }
+      ],
       items: [
         { title: 'Dashboard', icon: 'dashboard' },
         { title: 'Account', icon: 'account_box' },
         { title: 'Admin', icon: 'gavel' }
       ]
     }
-  }
+  },
+  methods: {
+    getAlgos() {
+      axios({
+        url: url.resolve(ML_ENDPOINT, '/jobs'),
+        responseType: 'json',
+      })
+      .then(res => {
+        const jobs = res.data.jobs
+        this.algos = jobs
+        console.log(res.data.jobs.map(job => job.algorithm_name))
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  created() {
+    this.getColumns()
+  },
 }
 </script>

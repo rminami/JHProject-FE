ea842e594f9bb884897c02c614df870e3081a0c4
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-09 18:58:46 +0100

    Added snackbar to display when the backend server is unavailable

---
+++ src/components/snackbars/BackendSnackbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-snackbar
+    :timeout="3000"
+    color="error"
+    v-model="isDisplayed"
+  >
+    Could not connect to back-end server.
+    <v-btn dark flat @click.native="$emit('input')">Close</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: ['isDisplayed'],
+  name: 'BackendSnackbar'
+}
+</script>

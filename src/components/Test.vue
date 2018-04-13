ed25fcaa60e4368cd68dc87901c2c939ca9be26a
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-13 15:17:15 +0100

    Added testing component

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex>
+        
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>

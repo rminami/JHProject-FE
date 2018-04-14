da5299675d852f855678d7bbc42bcd03e769b3af
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-14 03:32:14 +0100

    Added particles to login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,38 @@
 <template>
-  <v-container>
-    <v-layout row justify-space-around>
-      <v-flex xs5>
-        <v-card>
-          <v-card-title>
-            <span class="headline">Log in</span>
-          </v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                name="login"
-                label="Email address"
-                type="email"
-                :rules="[rules.required, rules.email]"
-              ></v-text-field>
-              <v-text-field
-                name="password"
-                label="Password"
-                :append-icon="pwdHidden ? 'visibility' : 'visibility_off'"
-                :append-icon-cb="() => (pwdHidden = !pwdHidden)"
-                :type="pwdHidden ? 'password' : 'text'"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn primary>Login</v-btn>
-            <v-btn secondary>Sign up</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div id="particles-js">
+    <v-container>
+      <v-layout row justify-space-around>
+        <v-flex xs5>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Log in</span>
+            </v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  name="login"
+                  label="Email address"
+                  type="email"
+                  :rules="[rules.required, rules.email]"
+                ></v-text-field>
+                <v-text-field
+                  name="password"
+                  label="Password"
+                  :append-icon="pwdHidden ? 'visibility' : 'visibility_off'"
+                  :append-icon-cb="() => (pwdHidden = !pwdHidden)"
+                  :type="pwdHidden ? 'password' : 'text'"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn primary>Login</v-btn>
+              <v-btn secondary>Sign up</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -47,6 +49,133 @@ export default {
         }
       }
     }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initParticleJS()
+    })
+  },
+  methods: {
+    initParticleJS() {
+      particlesJS('particles-js', {
+        "particles": {
+            "number": {
+              "value": 380,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#ff0000"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 3,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ff0000",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 6,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+              },
+              "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "grab"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "push"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 140,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 200,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+      })
+    }
   }
 }
+
 </script>
+
+<style>
+.canvas {
+  display: block;
+}
+
+#particles-js {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
+</style>

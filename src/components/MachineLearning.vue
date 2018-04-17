c2e28e447df8eb46791d69bbb7e9a54fd5378c63
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-17 01:34:31 +0100

    Made parent component compatible with BasicPanel, EstimatorPanel, and TransformerPanel

--- src/components/MachineLearning.vue
+++ src/components/MachineLearning.vue
@@ -6,19 +6,23 @@
           <input-file-panel
             :adv-enabled="advEnabled"
             @adv-toggle="toggleAdvOptions"
-            @error="beConnected = true"
+            @error="beConnected = false"
           />
+
+          <!-- Components for advanced mode -->
           <transformer-panel
             v-show="advEnabled"
             v-for="(transformer, index) in transformers"
             :key="index"
             :index="index"
-            :algos="tabularAlgos"
+            :jobs="transformerJobs"
             :values="transformer"
             @change="handleTransformChange"
             @append="appendTransformer"
             @remove="removeTransformer"
           />
+          <!-- A floating button to add more transformer steps is shown when
+          the advanced options are enabled. -->
           <v-btn
             v-show="advEnabled"
             id="add-step-btn"
@@ -30,11 +34,18 @@
             <v-icon>add</v-icon>
           </v-btn>
           <estimator-panel
-            :adv-enabled="advEnabled"
+            v-show="advEnabled"
             :index="transformers.length"
-            :algos="tabularAlgos"
+            :jobs="estimatorJobs"
           />
-          <next-panel/>
+          <!-- Components for basic mode -->
+          <basic-panel
+            v-show="!advEnabled"
+            :jobs="basicJobs"
+          />
+          <next-panel
+            :adv-enabled="advEnabled"
+          />
         </v-layout>
       </v-container>
     </v-layout>
@@ -53,8 +64,8 @@
 <script>
 import { mapState } from 'vuex'
 import axios from 'axios'
-import url from 'url'
 
+import BasicPanel from './ml/BasicPanel'
 import InputFilePanel from './ml/InputFilePanel'
 import EstimatorPanel from './ml/EstimatorPanel'
 import TransformerPanel from './ml/TransformerPanel'
@@ -63,18 +74,17 @@ import NextPanel from './ml/NextPanel'
 
 export default {
   components: {
+    'basic-panel': BasicPanel,
     'input-file-panel': InputFilePanel,
     'next-panel': NextPanel,
     'estimator-panel': EstimatorPanel,
     'transformer-panel': TransformerPanel
   },
-  computed: mapState({
-    beEndpoint: s => s.beEndpoint,
-    mlEndpoint: s => s.mlEndpoint
-  }),
   data() {
     return {
-      algos: [],
+      basicJobs: undefined,
+      transformerJobs: undefined,
+      estimatorJobs: undefined,
       transformers: [
         {
           name: '',
@@ -86,89 +96,222 @@ export default {
         parameters: {},
         hyperparams: {}
       },
-      beConnected: false,
-      mlConnected: false,
-      snackbarIsOpen: false,
-      advEnabled: true
-    }
-  },
-  watch: {
-    beConnected: function() {
-      this.snackbarIsOpen = this.beConnected || this.mlConnected
-    },
-    mlConnected: function() {
-      this.snackbarIsOpen = this.beConnected || this.mlConnected
+      beConnected: true,
+      mlConnected: true,
+      advEnabled: true,
+      snackbarIsOpen: false
     }
   },
   computed: {
-    tabularAlgos() {
-      return this.algos.filter(a => a.data_type === 'tabular')
-    },
+    /**
+     * The message shown on the snackbar is changed depending on whether or not
+     * the connection to the backend server fails, the connection to the machine
+     * learning server fails, or both.
+     */
     snackbarMsg() {
-      return ('Could not connect to '
-        + (this.beConnected ? 'backend' : '')
-        + ((this.beConnected && this.mlConnected) ? ' or ' : '')
-        + (this.mlConnected ? 'machine learning server' : '')
-        + '.')
+      return `${`Could not connect to
+        ${this.beConnected ? '' : 'backend'}
+        ${(this.beConnected || this.mlConnected) ? '' : ' or '}
+        ${this.mlConnected ? '' : 'machine learning server'}`
+      .trim()}.`
     },
+    /**
+     * Retrieves the backend and machine learning endpoints from Vuex
+     * centralized state.
+     */
     ...mapState({
       beEndpoint: s => s.beEndpoint,
       mlEndpoint: s => s.mlEndpoint
     })
   },
+  watch: {
+    /**
+     * snackbarIsOpen is updated everytime beConnected or mlConnect is changed;
+     * that is, whenever either a connection to backend or the machine learning
+     * server fails.
+     *
+     * This cannot be a computed property, as it must be set to
+     * false after the snackbar has been displayed for a certain amount of time.
+     */
+    beConnected() {
+      this.snackbarIsOpen = !this.beConnected || !this.mlConnected
+    },
+    mlConnected() {
+      this.snackbarIsOpen = !this.beConnected || !this.mlConnected
+    }
+  },
   methods: {
-    getAlgos() {
-      // url: 'https://to26.host.cs.st-andrews.ac.uk/JH-Project/machine-learning-api/1.0/jobs',
+    /**
+     * Retrieves a list of jobs available on the machine learning server.
+     *
+     * @see https://github.com/CS3099JH2017/cs3099jh/blob/master/protocols/ML01.md#listing-available-jobs
+     * for the protocol.
+     */
+    getJobs() {
       axios({
         method: 'get',
         baseURL: this.mlEndpoint,
         url: '/jobs',
-        responseType: 'json',
+        responseType: 'json'
       })
       .then(res => {
-        // const jobs = res.data.data.jobs
-        const jobs = res.data.jobs
-        this.algos = jobs
+        /**
+         * First, the jobs are filtered so that only those fit for use with
+         * tabular data are included.
+         *
+         * Then, the job object received from this endpoint is reformatted to
+         * comply with ML Group 7's protocol.
+         */
+        this.basicJobs = res.data.data.jobs
+        .filter(job => job.data_type === 'tabular')
+        .map(job => ({
+          name: job.algorithm_name, // algorithm_name is changed to name
+          job_description: job.job_description,
+          job_id: job.job_id,
+          parameters: job.parameters.map(param => ({
+            name: param.id,
+            // description is changed to info (actually, 'description' might be better)
+            info: param.description,
+            type: param.type,
+            required: param.required
+          })),
+          partial_training_support: job.partial_training_support
+        }))
       })
       .catch(err => {
-        this.mlConnected = true 
+        this.mlConnected = false
         console.log(err)
       })
     },
+    /**
+     * Retrieves a list of transformer jobs available on the machine learning
+     * server. This functionality is not part of the basic specifications.
+     *
+     * @see https://app.swaggerhub.com/apis/syzroy/ML7-API/1.1#/algorithms/transformers
+     * for the protocol.
+     */
+    getTransformers() {
+      if (!this.advEnabled) {
+        return
+      }
+      axios({
+        method: 'get',
+        baseURL: this.mlEndpoint,
+        url: '/transformers',
+        responseType: 'json'
+      })
+      .then(res => {
+        this.transformerJobs = res.data.data.jobs
+      })
+      .catch(err => {
+        this.mlConnected = false
+        console.log('Could not retrieve transformers.')
+        console.log(err)
+      })
+    },
+    /**
+     * Retrieves a list of estimator jobs available on the machine learning
+     * server. This functionality is not part of the basic specifications.
+     *
+     * @see https://app.swaggerhub.com/apis/syzroy/ML7-API/1.1#/algorithms/estimators
+     * for the protocol.
+     */
+    getEstimators() {
+      if (!this.advEnabled) {
+        return
+      }
+      axios({
+        method: 'get',
+        baseURL: this.mlEndpoint,
+        url: '/estimators',
+        responseType: 'json'
+      })
+      .then(res => {
+        console.log(res.data)
+        this.estimatorJobs = res.data.data.jobs
+      })
+      .catch(err => {
+        this.mlConnected = false
+        console.log('Could not retrieve estimators.')
+        console.log(err)
+      })
+    },
+    /**
+     * Concatinates a new transformer step to the end of the list.
+     */
     addTransformStep() {
       this.transformers.push({ selected: '', parameters: {} })
     },
-    appendTransformer(indexToAppendTo) {
+    /**
+     * Appends a new transformer step right after the step where the
+     * append event came from.
+     *
+     * @param {number} index - The index of the step to append to.
+     */
+    appendTransformer(index) {
       this.transformers = [
-        ...this.transformers.slice(0, indexToAppendTo),
+        ...this.transformers.slice(0, index + 1),
         { name: '', parameters: {} },
-        ...this.transformers.slice(indexToAppendTo)
+        ...this.transformers.slice(index + 1)
       ]
     },
+    /**
+     * Updates the transformers list in this component. It is triggered whenever
+     * a transfomer algorithm or parameter is changed.
+     *
+     * @param {Object} newTransformer - An object representing the updated step.
+     * @param {number} index - The index of the updated step.
+     */
     handleTransformChange(newTransformer, index) {
-      const prevStep = this.transformers[index]
       this.transformers = [
         ...this.transformers.slice(0, index),
         newTransformer,
         ...this.transformers.slice(index + 1)
       ]
     },
-    removeTransformer(idToRemove) {
+    /**
+     * Removes a step from the transformers list.
+     *
+     * @param {number} indexToRemove - The index of the step to remove.
+     */
+    removeTransformer(indexToRemove) {
       this.transformers = this.transformers.filter((step, index) => (
-        index !== idToRemove
+        index !== indexToRemove
       ))
     },
+    /**
+     * This function is called whenever the advanced options switch is toggled.
+     *
+     * The frontend tries to retrieve a list of available transformer and
+     * estimator jobs from the machine learning server when advanced options are
+     * enabled.
+     *
+     * @param {boolean} toggleTo - The value the switch was toggled to.
+     */
     toggleAdvOptions(toggleTo) {
       this.advEnabled = toggleTo
+      if (this.advEnabled) {
+        this.getTransformers()
+        this.getEstimators()
+      }
     }
   },
+  /**
+   * This function is run when the component is created i.e. when the machine
+   * learning page is loaded. It retrieves all of the information needed to
+   * populate the machine learning console.
+   */
   created() {
-    this.getAlgos()
+    this.getJobs()
+    if (this.advEnabled) {
+      this.getTransformers()
+      this.getEstimators()
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 #outer-container {
   overflow: auto;
   white-space: nowrap;

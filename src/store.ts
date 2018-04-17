d12cd9804b319115d6701548b5b35f39a0eab5f8
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-17 01:37:02 +0100

    Added ML Group 7's server as machine learning endpoint

--- src/store.ts
+++ src/store.ts
@@ -7,6 +7,7 @@ export default new Vuex.Store({
   state: {
     beEndpoint: 'http://127.0.0.1:4000',
     mlEndpoint: 'http://127.0.0.1:7000'
+    // mlEndpoint: 'https://to26.host.cs.st-andrews.ac.uk/JH-Project/machine-learning-api/1.0/'
   },
   mutations: {
 

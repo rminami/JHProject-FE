41145ad43a12a93a224c9dc754847c354e6f097c
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-17 01:28:33 +0100

    Created mixin for computing list of job names

---
+++ src/mixins/jobNameList.js
@@ -0,0 +1,15 @@
+/**
+ * Given a list of job objects as specified in the protocol, this mixin will
+ * return a list containing just their names for the selector.
+ */
+export default {
+  computed: {
+    jobNameList() {
+      try {
+        return this.jobs.map(a => a.name)
+      } catch (err) {
+        return []
+      }
+    }
+  }
+}

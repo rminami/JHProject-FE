fcfc059f810edc09bc0de5c0d5dc1dd5ac045ef2
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-20 02:27:37 +0100

    I think I'll make this into a standalone JavaScript file later

--- src/components/visualizations/ScatterPlot.vue
+++ src/components/visualizations/ScatterPlot.vue
@@ -1,142 +1,179 @@
 <template>
-
+  <div id="scatterplot-wrapper">
+    <svg id="svg" :width="width" :height="height"/>
+  </div>
 </template>
 
 <script>
-// import { select, event } from 'd3-selection';
-// import { scaleLinear } from 'd3-scale';
-// import { extent } from 'd3-array';
-// import { axisLeft, axisBottom } from 'd3-axis';
-// import { zoom, zoomIdentity } from 'd3-zoom';
+import { select, event } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
+import { extent } from 'd3-array';
+import { axisLeft, axisBottom } from 'd3-axis';
+import { zoom, zoomIdentity } from 'd3-zoom';
 
-// import { radius, zoomDur, maxZoom, tooltipLeft,
-//     tooltipTop, colors } from '@/assets/Constants';
+import { radius, zoomDur, maxZoom, tooltipLeft,
+    tooltipTop, colors } from '@/assets/Constants';
 
-// /**
-//  * Displays 2D scatter plot.
-//  *
-//  * @param {!Array<string>} headers - Array of header titles
-//  * @param {string} headers[0] - Header for x-axis.
-//  * @param {string} headers[1] - Header for y-axis.
-//  * @param {!number[][]} data - Data points to render scatter plot from
-//  * @param {number} data[][0] - Value for x-axis
-//  * @param {number} data[][1] - Value for y-axis
-//  */
-// export function displayScatterPlot(headers, data) {
-//     const zoomFunc = zoom().scaleExtent([1, maxZoom]);
-//     const svg = select('svg')
-//         .call(zoomFunc.transform, zoomIdentity)    // clears previous zoom level
-//         .call(zoomFunc.on('zoom', zoomed))         // zoom will be handled by function 'zoomed'
-//         .on('dblclick.zoom', null);                // disables double click to zoom
+export default {
+  name: 'ScatterPlot',
+  data() {
+    return {
+      width: 640,
+      height: 480
+    }
+  },
+  mounted() {
+    // this.renderScatterPlot(this.headers, this.data)
 
-//     const margin = {top: 30, bottom: 80, left: 80, right: radius};
-//     const width = +svg.attr('width') - margin.left - margin.right;
-//     const height = +svg.attr('height') - margin.top - margin.bottom;
-//     const g = svg.append('g')
-//         .attr('transform', 'translate(' + margin.left + ', '
-//             + margin.top + ')');
+    // this.width = Math.max(document.getElementById('scatterplot-wrapper').clientWidth - 80, 0)
+    // this.height = Math.max(document.getElementById('scatterplot-wrapper').clientHeight - 50, 0)
 
-//     const x = scaleLinear()
-//         .range([radius, width - radius])
-//         .domain(extent(data, d => d[0]));
-//     const y = scaleLinear()
-//         .range([height - radius, radius])
-//         .domain(extent(data, d => d[1]));
+    // console.log(`Width: ${this.width}, Height: ${this.height}`)
+  },
+  methods: {
+    /**
+     * Displays 2D scatter plot.
+     *
+     * @param {string[]} headers - Array of header titles.
+     *         headers[0] = x-axis, headers[1] = y-axis.
+     * @param {number[][]} data - Data points to render scatter plot from.
+     *         data[][0] = x-axis values, data[][1] = y-axis values.
+     */
+    renderScatterPlot(headers, data) {
+      this.width = document.getElementById('scatterplot-wrapper').clientWidth
+      this.height = document.getElementById('scatterplot-wrapper').clientHeight
 
-//     let zoomX = x;
-//     let zoomY = y;
+      // console.log(`Width: ${this.width}, Height: ${this.height}`)
 
-//     const circleGroup = g.append('g')
-//         .attr('class', 'circles')
-//         .attr('clip-path', 'url(#clip)');
+      const zoomFunc = zoom().scaleExtent([1, maxZoom]);
+      const svg = select('#svg')
+        .html('') // empties all previous children
+        .call(zoomFunc.transform, zoomIdentity) // clears previous zoom level
+        .call(zoomFunc.on('zoom', zoomed)) // zoom will be handled by function 'zoomed'
+        .on('dblclick.zoom', null); // disables double click to zoom
 
-//     /* Adds clip path to hide data points outside axes */
-//     circleGroup.append('defs')
-//         .append('clipPath')
-//         .attr('id', 'clip')
-//         .append('rect')
-//         .attr('x', 0)
-//         .attr('y', 0)
-//         .attr('width', width)
-//         .attr('height', height);
+      const margin = {
+        top: 30,
+        bottom: 80,
+        left: 80,
+        right: radius
+      };
+      const width = this.width - margin.left - margin.right;
+      const height = this.height - margin.top - margin.bottom;
+      const g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ', ' +
+          margin.top + ')');
 
-//     const circles = circleGroup.selectAll('circle')
-//         .data(data)
-//         .enter()
-//         .append('circle')
-//         .attr('r', radius)
-//         .attr('opacity', 0.7)
-//         .attr('cx', d => x(d[0]))
-//         .attr('cy', d => y(d[1]))
-//         .style('fill', d => colors(d[3]))
-//         .on('mouseover', d => {
-//             select('#x-tooltip-value').text(d[0]);
-//             select('#y-tooltip-value').text(d[1]);
-//             select('#c-tooltip-value').text(d[3]);
-//             for (let i = 4; i < headers.length; ++i) {
-//                 select('#tooltip-value-' + i).text(d[i]);
-//             }
+      const x = scaleLinear()
+        .range([radius, width - radius])
+        .domain(extent(data, d => d[0]));
+      const y = scaleLinear()
+        .range([height - radius, radius])
+        .domain(extent(data, d => d[1]));
 
-//             const xTooltipPos = zoomX(d[0]) + margin.left + radius + tooltipLeft;
-//             const yTooltipPos = zoomY(d[1]) + margin.top + radius + tooltipTop;
+      let zoomX = x;
+      let zoomY = y;
 
-//             select('#tooltip')
-//                 .style('left', xTooltipPos + 'px')
-//                 .style('top', yTooltipPos + 'px')
-//                 .classed('invisible', false);
+      const circleGroup = g.append('g')
+        .attr('class', 'circles')
+        .attr('clip-path', 'url(#clip)');
 
-//         }).on('mouseout', () => {
-//             select('#tooltip').classed('invisible', true);
-//         });
+      /* Adds clip path to hide data points outside axes */
+      circleGroup.append('defs')
+        .append('clipPath')
+        .attr('id', 'clip')
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', width)
+        .attr('height', height);
 
-//     /* x-axis */
-//     const xFunc = axisBottom(x);
-//     const xAxis = g.append('g')
-//         .attr('class', 'x-axis')
-//         .attr('transform', 'translate(0,' + height + ')')
-//         .call(xFunc);
+      const circles = circleGroup.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('r', radius)
+        .attr('opacity', 0.7)
+        .attr('cx', d => x(d[0]))
+        .attr('cy', d => y(d[1]))
+        .style('fill', d => colors(d[3]))
+        .on('mouseover', d => {
+          select('#x-tooltip-value').text(d[0]);
+          select('#y-tooltip-value').text(d[1]);
+          select('#c-tooltip-value').text(d[3]);
+          for (let i = 4; i < headers.length; ++i) {
+            select('#tooltip-value-' + i).text(d[i]);
+          }
 
-//     /* label for x-axis */
-//     g.append('text')
-//         .attr('class', 'axis-label x-axis-label')
-//         .attr('transform', 'translate(' + (width / 2) + ','
-//             + (height + margin.top + 10) + ')')
-//         .style('text-anchor', 'middle')
-//         .text(headers[0]);
+          const xTooltipPos = zoomX(d[0]) + margin.left + radius + tooltipLeft;
+          const yTooltipPos = zoomY(d[1]) + margin.top + radius + tooltipTop;
 
-//     /* y-axis */
-//     const yFunc = axisLeft(y);
-//     const yAxis = g.append('g')
-//         .attr('class', 'y-axis')
-//         .call(yFunc);
+          select('#tooltip')
+            .style('left', xTooltipPos + 'px')
+            .style('top', yTooltipPos + 'px')
+            .classed('invisible', false);
 
-//     /* label for y-axis */
-//     g.append('text')
-//         .attr('class', 'axis-label y-axis-label')
-//         .attr('transform', 'rotate(-90)')
-//         .attr('y', 0 - margin.left + 10)
-//         .attr('x', 0 - (height / 2))
-//         .attr('dy', '1em')
-//         .style('text-anchor', 'middle')
-//         .text(headers[1]);
+        }).on('mouseout', () => {
+          select('#tooltip').classed('invisible', true);
+        });
 
-//     /** Zoom function for the scatter plot. */
-//     function zoomed() {
-//         xAxis.transition()
-//             .duration(zoomDur)
-//             .call(xFunc.scale(event.transform.rescaleX(x)));
+      /* x-axis */
+      const xFunc = axisBottom(x);
+      const xAxis = g.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xFunc);
 
-//         yAxis.transition()
-//             .duration(zoomDur)
-//             .call(yFunc.scale(event.transform.rescaleY(y)));
+      /* label for x-axis */
+      g.append('text')
+        .attr('class', 'axis-label x-axis-label')
+        .attr('transform', 'translate(' + (width / 2) + ',' +
+          (height + margin.top + 10) + ')')
+        .style('text-anchor', 'middle')
+        .text(headers[0]);
 
-//         /* redraw circles to new scale */
-//         zoomX = event.transform.rescaleX(x);
-//         zoomY = event.transform.rescaleY(y);
+      /* y-axis */
+      const yFunc = axisLeft(y);
+      const yAxis = g.append('g')
+        .attr('class', 'y-axis')
+        .call(yFunc);
 
-//         circles.attr('cx', d => zoomX(d[0]))
-//             .attr('cy', d => zoomY(d[1]));
-//     }
-// }
+      /* label for y-axis */
+      g.append('text')
+        .attr('class', 'axis-label y-axis-label')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left + 10)
+        .attr('x', 0 - (height / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text(headers[1]);
+
+      /** Zoom function for the scatter plot. */
+      function zoomed() {
+        xAxis.transition()
+          .duration(zoomDur)
+          .call(xFunc.scale(event.transform.rescaleX(x)));
+
+        yAxis.transition()
+          .duration(zoomDur)
+          .call(yFunc.scale(event.transform.rescaleY(y)));
+
+        /* redraw circles to new scale */
+        zoomX = event.transform.rescaleX(x);
+        zoomY = event.transform.rescaleY(y);
+
+        circles.attr('cx', d => zoomX(d[0]))
+          .attr('cy', d => zoomY(d[1]));
+      }
+    }
+  }
+}
 
 </script>
+
+<style lang="stylus" scoped>
+#scatterplot-wrapper
+  display: inline-block
+  width: 100%
+  height: 100%
+</style>

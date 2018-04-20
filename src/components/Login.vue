7591fbe26ca57d0d212b778c8b395b234bf180d0
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-20 02:24:25 +0100

    Moved particles to external file

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,30 +7,33 @@
             <v-card-title>
               <span class="headline">Log in</span>
             </v-card-title>
-            <v-card-text>
+            <v-card-text class="card-text">
               <v-form>
                 <v-text-field
                   name="login"
-                  label="Email address"
-                  type="email"
+                  label="Username"
+                  v-model="username"
+                  type="text"
                   autocomplete="username"
-                  :rules="[rules.required, rules.email]"
-                  aria-label="Email address"
+                  :rules="[rules.required]"
+                  aria-label="Username"
                 ></v-text-field>
                 <v-text-field
                   name="password"
                   label="Password"
+                  v-model="password"
                   autocomplete="current-password"
                   :append-icon="pwdHidden ? 'visibility' : 'visibility_off'"
                   :append-icon-cb="() => (pwdHidden = !pwdHidden)"
                   :type="pwdHidden ? 'password' : 'text'"
+                  :rules="[rules.required]"
                   aria-label="Password"
                 ></v-text-field>
               </v-form>
             </v-card-text>
             <v-card-actions>
-              <v-btn color="primary">Login</v-btn>
-              <v-btn color="secondary">Sign up</v-btn>
+              <v-btn color="primary" @click.prevent="login">Login</v-btn>
+              <v-btn color="secondary" to="/signup">Sign up</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -40,105 +43,25 @@
 </template>
 
 <script>
-/* global particlesJS */
+import particlesMixin from '@/mixins/particlesMixin'
+import validatorMixin from '@/mixins/validatorMixin'
+import { AUTH_REQUEST } from '@/store/actions';
 
 export default {
+  mixins: [particlesMixin, validatorMixin],
   data() {
     return {
-      email: '',
+      username: '',
+      password: '',
       pwdHidden: true,
-      rules: {
-        required: value => !!value || 'Required.',
-        email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        }
-      }
     }
   },
-  mounted() {
-    this.initParticleJS()
-  },
   methods: {
-    initParticleJS() {
-      particlesJS('particles', {
-        particles: {
-          number: {
-            value: 200
-          },
-          color: {
-            value: '#191970'
-          },
-          shape: {
-            type: 'square',
-            stroke: {
-              width: 1,
-              color: '#cccccc'
-            }
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1
-            }
-          },
-          size: {
-            value: 50
-          },
-          line_linked: {
-            enable: true,
-            distance: 100,
-            color: '#ffffff',
-            opacity: 0.4,
-            width: 2
-          },
-          move: {
-            enable: true,
-            speed: 3
-          }
-        },
-        interactivity: {
-          detect_on: 'canvas',
-          events: {
-            onhover: {
-              enable: false,
-              mode: 'repulse'
-            },
-            onclick: {
-              enable: true,
-              mode: 'push'
-            },
-            resize: true
-          },
-          modes: {
-            grab: {
-              distance: 100,
-              line_linked: {
-                opacity: 5,
-                color: '#d45b0b',
-                size: {
-                  value: 20
-                }
-              }
-            },
-            bubble: {
-              distance: 50,
-              size: 20,
-              duration: 2,
-              opacity: 8,
-              speed: 3
-            },
-            repulse: {
-              distance: 10,
-              duration: 0.4
-            },
-            push: {
-              particles_nb: 4
-            }
-          }
-        }
+    login() {
+      const { username, password } = this
+      this.$store.dispatch(AUTH_REQUEST, { username, password })
+      .then(() => {
+        this.$router.push('/')
       })
     }
   }
@@ -165,5 +88,9 @@ export default {
   max-width: 90%
   min-width: 40%
   padding: 2em 3em
+
+.card-text
+  padding-left 24px
+  padding-right 24px
 
 </style>

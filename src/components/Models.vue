2c145ea4ae966410a8ea9c369545432b29b94854
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-20 17:48:38 +0100

    Getting a list of machine learning models

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -3,7 +3,7 @@
   <v-container xs10 offset-xs1>
     <v-card>
       <v-card-title>
-        <h2 class="headline">Users</h2>
+        <h2 class="headline">Machine Learning Models</h2>
         <v-spacer></v-spacer>
         <v-text-field
           append-icon="search"
@@ -14,27 +14,16 @@
         ></v-text-field>
       </v-card-title>
       <v-data-table
-        v-model="selected"
         :headers="headers"
         :items="items"
-        select-all
         :pagination.sync="pagination"
         :search="search"
-        item-key="email"
+        item-key="model_id"
         class="elevation-1"
         v-show="items.length > 0"
       >
         <template slot="headers" slot-scope="props">
           <tr>
-            <th>
-              <v-checkbox
-                primary
-                hide-details
-                @click.native="toggleAll"
-                :input-value="props.all"
-                :indeterminate="props.indeterminate"
-              ></v-checkbox>
-            </th>
             <th
               v-for="header in props.headers"
               :key="header.text"
@@ -48,86 +37,48 @@
           </tr>
         </template>
         <template slot="items" slot-scope="props">
-          <tr :active="props.selected" @click="props.selected = !props.selected">
-            <td>
-              <v-checkbox
-                primary
-                hide-details
-                :input-value="props.selected"
-              ></v-checkbox>
-            </td>
-            <td>{{ props.item.firstname }}</td>
-            <td>{{ props.item.lastname }}</td>
-            <td>{{ props.item.email }}</td>
+          <tr :active="props.selected" @click="props.expanded = !props.expanded">
+            <td>{{ props.item.job_id }}</td>
+            <td>{{ props.item.model_id }}</td>
             <td>
               <v-chip
                 label
                 small
-                :color="`${props.item.role === 'Admin' ? 'red' : 'blue'} lighten-4`"
+                :color="statusToColor(props.item.status)"
                 class="ml-0"
               >
-                {{ props.item.role }}
+                {{ capitalize(props.item.status) }}
               </v-chip>
             </td>
-            <td class="justify-center layout px-0">
-              <v-btn icon class="mx-0" @click.stop="editItem(props.item)">
-                <v-icon color="grey">edit</v-icon>
+            <td class="justify-center">{{ Math.round(+props.item.percent_trained * 100) + '%' }}</td>
+
+            <td v-if="props.item.status === 'running'" class="justify-center layout px-0">
+              <v-btn icon class="mx-0" @click.stop="() => 0">
+                <v-icon color="grey">pause</v-icon>
+              </v-btn>
+              <v-btn icon class="mx-0" @click.stop="() => 0">
+                <v-icon color="grey">stop</v-icon>
+              </v-btn>
+            </td>
+            <td v-else class="justify-center layout px-0">
+              <v-btn icon class="mx-0" @click.stop="() => 0">
+                <v-icon color="grey">delete</v-icon>
               </v-btn>
             </td>
           </tr>
+        </template>
+        <template slot="expand" slot-scope="props">
+          <v-card flat>
+            <v-card-text>
+              <p><strong>Description: </strong>{{ props.item.description }}</p>
+            </v-card-text>
+          </v-card>
         </template>
         <v-alert slot="no-results" :value="true" color="error" icon="warning">
           No results for "{{ search }}".
         </v-alert>
       </v-data-table>
     </v-card>
-
-    <!-- Dialog for changing user information. -->
-    <v-dialog v-model="dialog" max-width="500px">
-      <v-card>
-        <v-toolbar>
-          <v-toolbar-title>Edit User</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="close">
-            <v-icon>close</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-card-text>
-          <v-container>
-            <v-form>
-              <v-text-field label="First Name" v-model="editedItem.firstname" @keyup.enter="save"></v-text-field>
-              <v-text-field label="Last Name" v-model="editedItem.lastname" @keyup.enter="save"></v-text-field>
-              <v-text-field label="email" v-model="editedItem.email" @keyup.enter="save"></v-text-field>
-              <v-select label="Role" v-model="editedItem.role" :items="roles">
-                <template slot="selection" slot-scope="role">
-                  <v-chip
-                    label
-                    :color="`${role.item === 'Admin' ? 'red' : 'blue'} lighten-4`"
-                    class="ml-0"
-                  >
-                    {{ role.item }}
-                  </v-chip>
-                </template>
-                <template slot="item" slot-scope="role">
-                  <v-chip
-                    label
-                    :color="`${role.item === 'Admin' ? 'red' : 'blue'} lighten-4`"
-                    class="ml-0"
-                  >
-                    {{ role.item }}
-                  </v-chip>
-                </template>
-              </v-select>
-            </v-form>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="secondary" flat @click.native="close">Cancel</v-btn>
-          <v-btn color="primary" flat @click.native="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-container>
 </template>
 
@@ -141,61 +92,80 @@ export default {
     },
     search: '',
     selected: [],
+    // "percent_trained": 0.3,
+    // "status": "running",
+    // "description": "string",
+    // "job_id": "string",
+    // "model_id": "string",
+    // "start_time": "string",
+    // "started_by": "string"
     headers: [
-      { text: 'First Name', value: 'firstname' },
-      { text: 'Last Name', value: 'lastname' },
-      { text: 'Email', value: 'email' },
-      { text: 'Role', value: 'role' },
-      { text: 'Edit', value: 'edit' }
+      { text: 'Job ID', value: 'job_id' },
+      { text: 'Model ID', value: 'model_id' },
+      { text: 'Status', value: 'status' },
+      { text: '% trained', value: 'percent_trained' },
+      { text: 'Actions', value: 'actions' }
     ],
+    status: ['running', 'complete', 'failed'],
     roles: ['User', 'Admin'],
-    items: [],
-    editedIndex: -1,
-    editedItem: {
-      value: false,
-      firstname: '',
-      lastname: '',
-      email: '',
-      role: 'User'
-    },
-    defaultItem: {
-      value: false,
-      firstname: '',
-      lastname: '',
-      email: '',
-      role: 'User'
-    },
+    items: [
+      {
+        expanded: false,
+        job_id: 'job123',
+        model_id: 'model1234',
+        status: 'complete',
+        description: 'This model does stuff',
+        percent_trained: 1
+      },
+      {
+        expanded: false,
+        job_id: 'job456',
+        model_id: 'model4567',
+        status: 'running',
+        description: 'This model is good',
+        percent_trained: 0.78
+      },
+      {
+        expanded: false,
+        job_id: 'job789',
+        model_id: 'model09834',
+        status: 'failed',
+        description: 'This model is useless',
+        percent_trained: 0
+      }
+    ],
     dialog: false
   }),
   created() {
-    axios.get('https://randomuser.me/api', {
-      params: { results: 42 }
-    })
-    .then(res => {
-      this.items = res.data.results.map(user => ({
-        firstname: this.capitalize(user.name.first),
-        lastname: this.capitalize(user.name.last),
-        email: user.email,
-        role: Math.random() > 0.1 ? 'User' : 'Admin'
-      }))
-    })
-    .catch(err => {
-      console.log('Could not get users!')
-    })
+    // axios.get('https://randomuser.me/api', {
+    //   params: { results: 42 }
+    // })
+    // .then(res => {
+    //   this.items = res.data.results.map(user => ({
+    //     firstname: this.capitalize(user.name.first),
+    //     lastname: this.capitalize(user.name.last),
+    //     email: user.email,
+    //     role: Math.random() > 0.1 ? 'User' : 'Admin'
+    //   }))
+    // })
+    // .catch(err => {
+    //   console.log('Could not get users!')
+    // })
   },
 
   methods: {
     capitalize(str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
     },
-    editItem (item) {
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-    toggleAll () {
-      if (this.selected.length) this.selected = []
-      else this.selected = this.items.slice()
+    statusToColor(status) {
+      switch(status) {
+        case 'running':
+          return 'blue accent-1'
+        case 'complete':
+          return 'light-green accent-1'
+        case 'failed':
+          return 'red accent-1'
+      }
     },
     changeSort (column) {
       if (this.pagination.sortBy === column) {
@@ -205,6 +175,10 @@ export default {
         this.pagination.descending = false
       }
     },
+    toggleAll () {
+      if (this.selected.length) this.selected = []
+      else this.selected = this.items.slice()
+    },
     close () {
       this.dialog = false
       setTimeout(() => {
@@ -212,15 +186,6 @@ export default {
         this.editedIndex = -1
       }, 300)
     },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.items[this.editedIndex], this.editedItem)
-      } else {
-        this.items.push(this.editedItem)
-      }
-      this.close()
-    }
   }
 }
 </script>

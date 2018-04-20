1fc14e461d2fd171527aea66132c3f87784f7672
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-20 02:30:24 +0100

    Added more endpoints for testing

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,24 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: localStorage.getItem('user-token') || '',
+    status: '',
     beEndpoint: 'http://127.0.0.1:4000',
+    // beEndpoint: 'https://jm354.host.cs.st-andrews.ac.uk', // Jordan's backend
+    // beEndpoint: 'https://rw86.host.cs.st-andrews.ac.uk/be/hci1', // Ryan's backend
+    // mlEndpoint: 'http://e0248507.ngrok.io/JH-Project/machine-learning-api/1.0/' // Daphne's ML
     mlEndpoint: 'http://127.0.0.1:7000'
-    // mlEndpoint: 'https://to26.host.cs.st-andrews.ac.uk/JH-Project/machine-learning-api/1.0/'
+    // mlEndpoint: 'https://to26.host.cs.st-andrews.ac.uk/JH-Project/machine-learning-api/1.0/' // Tom's ML
   },
   mutations: {
 
   },
   actions: {
-
+    
   },
-});
+})

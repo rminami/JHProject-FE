a21813c831dc225c00e78b1aac5f8254421a80c2
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-21 15:04:44 +0100

    Added window for viewing all available projects

---
+++ src/components/Projects.vue
@@ -0,0 +1,101 @@
+<template>
+<v-container xs10 offset-xs1>
+  <v-list two-line>
+
+    <v-subheader inset>Projects</v-subheader>
+    <v-list-tile avatar v-for="project in projects" :key="project" @click="chooseProject(project)">
+      <v-list-tile-avatar>
+        <v-icon class="grey lighten-1 white--text">assignment</v-icon>
+      </v-list-tile-avatar>
+      <v-list-tile-content>
+        <v-list-tile-title>{{ project }}</v-list-tile-title>
+      </v-list-tile-content>
+      <v-list-tile-action @click.stop>
+        <v-menu bottom left>
+          <v-btn icon slot="activator" dark ripple>
+            <v-icon color="grey lighten-1">more_vert</v-icon>
+          </v-btn>
+          <v-list>
+            <v-list-tile
+              @click.stop="() => 0"
+            >
+              <v-list-tile-title>Download</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile
+            >
+              <v-list-tile-title>Rename</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </v-list-tile-action>
+    </v-list-tile>
+  </v-list>
+
+  <!-- <v-dialog v-model="textDialog" max-width="500px">
+    <rename-dialog
+      :prevFileName="selectedFileName"
+      @submit="renameFile"
+      @close="textDialog = false"
+    />
+  </v-dialog> -->
+
+  <!-- Snackbar is displayed when connection to backend fails -->
+  <v-snackbar
+    :timeout="3000"
+    color="error"
+    v-model="backendSnackbar"
+  >
+    Could not connect to backend.
+    <v-btn dark flat @click.native="backendSnackbar = false">Close</v-btn>
+  </v-snackbar>
+</v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import axios from 'axios'
+import url from 'url'
+import path from 'path'
+
+export default {
+  data() {
+    return {
+      textDialog: false,
+      projects: [],
+      backendSnackbar: false
+    }
+  },
+  created() {
+    axios({
+      baseURL: this.beEndpoint,
+      url: '/projects',
+    })
+    .then(res => {
+      console.log(res.data)
+      this.projects = res.data.data.map(project => project.project_name)
+    })
+    .catch(err => {
+      console.log(err.response)
+    })
+  },
+  computed: {
+    ...mapState({
+      beEndpoint: s => s.beEndpoint
+    })
+  },
+  methods: {
+    toggleDialog() {
+      this.textDialog = !this.textDialog
+    },
+    chooseProject(projectName) {
+      // this.$state.dispatch()
+      this.$router.push('/projects/' + projectName + '/files')
+    }
+  },
+  name: 'Projects'
+}
+</script>
+
+<style scoped>
+
+</style>

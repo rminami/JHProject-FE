8dde04629277db1105f07f51f155e1ab8443a652
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-21 15:09:01 +0100

    Added access tokens to axios defaults

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,21 @@ import store from './store'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import axios from 'axios'
 
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
+const accessToken = localStorage.getItem('access-token')
+if (accessToken) {
+  // Using both British and American spellings just in case
+  axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+  axios.defaults.headers.common.Authorisation = `Bearer ${accessToken}`
+}
+
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app')

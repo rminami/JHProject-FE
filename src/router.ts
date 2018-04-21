a3ae9a5e1093f27237e64c8acc8903802b92c8c9
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-21 15:08:37 +0100

    Remapped routes to include project names

--- src/router.ts
+++ src/router.ts
@@ -1,59 +1,97 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router, { Route } from 'vue-router'
+import store from './store'
+
+const isLoggedIn = (to: Route, from: Route, next: Function): void => {
+  if (store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/login')
+}
+
+const isNotLoggedIn = (to: Route, from: Route, next: Function): void => {
+  if (!store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/projects')
+}
 
 const routerOptions = [
   {
-    path: '/test',
-    component: 'Test'
-  },
-  {
     path: '/',
-    redirect: '/files'
+    redirect: '/projects'
   },
   {
-    path: '/files*.csv',
-    component: 'DataView'
+    path: '/projects',
+    component: 'Projects',
+    beforeEnter: isLoggedIn
   },
   {
-    path: '/files*.(png|jpg|dzi)',
-    component: 'ImageView'
+    path: '/projects/:project_name/*.csv',
+    component: 'DataView',
+    beforeEnter: isLoggedIn
   },
   {
-    path: '/files*.*',
-    component: 'RawFileView'
+    path: '/projects/:project_name/*.(png|jpg|dzi)',
+    component: 'ImageView',
+    beforeEnter: isLoggedIn
   },
   {
-    path: '/files*',
-    component: 'FileBrowse'
+    path: '/projects/:project_name/*.*',
+    component: 'RawFileView',
+    beforeEnter: isLoggedIn
+  },
+  {
+    path: '/projects/:project_name/*',
+    component: 'FileBrowse',
+    beforeEnter: isLoggedIn
   },
   {
     path: '/ml',
-    component: 'MachineLearning'
+    component: 'MachineLearning',
+    beforeEnter: isLoggedIn
   },
   {
     path: '/admin/console',
-    component: 'Admin'
+    component: 'Admin',
+    beforeEnter: isLoggedIn
   },
   {
     path: '/admin/users',
-    component: 'admin/UserControls'
+    component: 'admin/UserControls',
+    beforeEnter: isLoggedIn
   },
   {
     path: '/admin/server-settings',
-    component: 'settings/ServerSettings'
+    component: 'settings/ServerSettings',
+    beforeEnter: isLoggedIn
+  },
+  {
+    path: '/models',
+    component: 'Models',
+    beforeEnter: isLoggedIn
+  },
+  {
+    path: '/models/prediction/',
+    component: 'Prediction',
+    beforeEnter: isLoggedIn
   },
   {
     path: '/login',
-    component: 'Login'
+    component: 'Login',
+    beforeEnter: isNotLoggedIn
   },
   {
     path: '/signup',
-    component: 'Signup'
+    component: 'Signup',
+    beforeEnter: isNotLoggedIn
   },
   {
     path: '/settings',
     component: 'Settings'
-  },
+  }
 ]
 
 const routes = routerOptions.map(route => ({
